d1382a5286c8ef05d123417348d73eccaed1aaa6
Thomas Nguyen <tealeafnguyen@gmail.com>
2018-12-31 17:25:11 -0800

    small navbar fix

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -2,16 +2,21 @@ var modal = document.getElementById('myModal');
 
 var span = document.getElementsByClassName("close")[0];
 
-
-span.onclick = function () {
+$(document).on('click', 'span', function(){
     modal.style.display = "none";
-}
+    document.getElementById('contributeModal').style.display = 'none';
+})
 
 window.onclick = function (event) {
-    if (event.target == modal) {
+    if (event.target == modal || document.getElementById('contributeModal') == event.target) {
         modal.style.display = "none";
+        document.getElementById('contributeModal').style.display = 'none';
     }
 }
+
+$(document).on('click', '.stuffdori', function(){
+    document.getElementById('contributeModal').style.display = 'block';
+})
 
 $(document).on('click', '.news', function(){
     modal.style.display = 'block';
